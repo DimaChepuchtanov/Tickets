d71ac8fa8228756d9f055bdddb74aa061826b47d
Dima Chepushtanov <chepushtanovdima@yandex.ru>
2024-02-05 13:06:37 +0500

    update

--- transport.py
+++ transport.py
@@ -102,16 +102,78 @@ def all_marshurts_avia(language):
     return list(answer)
 
 
-def all_marshutrs_train():
+def all_marshutrs_train(language):
     """Функция получения всех маршрутов ЖД"""
 
-    return list(marshrut_train.values())
+    tickets = {}
+
+    if language == "en":
+        cursor.execute("SELECT * FROM ticketairen")
+        answer = cursor.fetchall()
+
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+
+        answer = tickets.values()
+    else:
+        cursor.execute("SELECT * FROM ticketairru")
+        answer = cursor.fetchall()
+
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+
+        answer = tickets.values()
+
+    return list(answer)
 
 
-def all_marshurts_bus():
+def all_marshurts_bus(language):
     """Функция получения всех маршрутов автобусов"""
 
-    return list(marshrut_bus.values())
+    tickets = {}
+
+    if language == "en":
+        cursor.execute("SELECT * FROM ticketbusen")
+        answer = cursor.fetchall()
+
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+
+        answer = tickets.values()
+    else:
+        cursor.execute("SELECT * FROM ticketbusru")
+        answer = cursor.fetchall()
+
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+        
+        answer = tickets.values()
+
+    return list(answer)
 
 
 def insert_marshrut_avia(newMarshrut):
@@ -200,52 +262,196 @@ def insert_marshrut_avia(newMarshrut):
 def insert_marshrut_train(newMarshrut):
     """Функция добаления нового маршрута ЖД"""
 
-    for key, value in marshrut_train.items():
-        if value["en"] == newMarshrut['en'] and value['ru'] == newMarshrut['ru']:
-            abort(400, "Маршрут уже создан")
+    cursor.execute(f"""SELECT id
+                      FROM tickettranien
+                      WHERE start = '{newMarshrut['en']['start']}' AND 
+                            "end" = '{newMarshrut['en']['end']}' AND
+                            "startDate" = '{datetime.strptime(newMarshrut['en']['start-date'], '%Y-%m-%d').date()}' AND
+                            "endDate" = '{datetime.strptime(newMarshrut['en']['end-date'], '%Y-%m-%d').date()}' AND
+                            "price" = '{newMarshrut['en']['price']}'""")
+    answer_en = cursor.fetchall()
 
-    keys = int(list(marshrut_train.keys())[-1]) + 1
+    cursor.execute(f"""SELECT id
+                      FROM tickettraniru
+                      WHERE start = '{newMarshrut['ru']['start']}' AND 
+                            "end" = '{newMarshrut['ru']['end']}' AND
+                            "startDate" = '{datetime.strptime(newMarshrut['ru']['start-date'], '%Y-%m-%d').date()}' AND
+                            "endDate" = '{datetime.strptime(newMarshrut['ru']['end-date'], '%Y-%m-%d').date()}' AND
+                            "price" = '{newMarshrut['ru']['price']}'""")
+    answer_ru = cursor.fetchall()
 
-    marshrut_train[keys] = {
-            "en": newMarshrut['en'],
-            "ru": newMarshrut['ru']
-        }
-    return marshrut_train[keys], 201
+    if len(answer_en) > 0 and len(answer_ru) > 0:
+        abort(400, "Маршрут уже создан")
+
+    if len(answer_ru) == 0 and len(answer_en) > 0:
+        try:
+            cursor.execute("SELECT MAX(id) FROM tickettraniru")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+
+            cursor.execute(f"""INSERT INTO tickettraniru(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['ru']['start']}', '{newMarshrut['ru']['end']}',
+                                '{datetime.strptime(newMarshrut['ru']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['ru']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['ru']['start-time']}', '{newMarshrut['ru']['end-time']}',
+                                {newMarshrut['ru']['price'].split(",")[0]})""")
+
+            cn.commit()
+            return newMarshrut['ru'], 201
+        except Exception as e:
+            abort(400, f"Ошибка добавления данных! Ошибка: {e}")
+    
+    if len(answer_ru) > 0 and len(answer_en) == 0:
+        try:
+            cursor.execute("SELECT MAX(id) FROM tickettranien")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+
+            cursor.execute(f"""INSERT INTO tickettraniru(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['en']['start']}', '{newMarshrut['en']['end']}',
+                                '{datetime.strptime(newMarshrut['en']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['en']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['en']['start-time']}', '{newMarshrut['en']['end-time']}',
+                                {newMarshrut['en']['price'].split(",")[0]})""")
+            cn.commit()
+            return newMarshrut['en'], 201
+        except Exception as e:
+            abort(400, f"Ошибка добавления данных! Ошибка: {e}")
+    else:
+        try:
+            cursor.execute("SELECT MAX(id) FROM tickettraniru")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+            cursor.execute(f"""INSERT INTO tickettraniru(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['ru']['start']}', '{newMarshrut['ru']['end']}',
+                                '{datetime.strptime(newMarshrut['ru']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['ru']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['ru']['start-time']}', '{newMarshrut['ru']['end-time']}',
+                                {newMarshrut['ru']['price'].split(",")[0]})""")
+
+            cursor.execute("SELECT MAX(id) FROM tickettranien")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+            cursor.execute(f"""INSERT INTO tickettranien(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['en']['start']}', '{newMarshrut['en']['end']}',
+                                '{datetime.strptime(newMarshrut['en']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['en']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['en']['start-time']}', '{newMarshrut['en']['end-time']}',
+                                {newMarshrut['en']['price'].split(",")[0]})""")
+            cn.commit()
+            return newMarshrut, 201
+        except Exception as e:
+            abort(400, f"Ошибка добавления данных! Ошибка: {e}")
 
 
 def insert_marshrut_bus(newMarshrut):
     """Функция добаления нового маршрута Автобус"""
 
-    for key, value in marshrut_bus.items():
-        if value["en"] == newMarshrut['en'] and value['ru'] == newMarshrut['ru']:
-            abort(400, "Маршрут уже создан")
+    cursor.execute(f"""SELECT id
+                      FROM ticketbusen
+                      WHERE start = '{newMarshrut['en']['start']}' AND 
+                            "end" = '{newMarshrut['en']['end']}' AND
+                            "startDate" = '{datetime.strptime(newMarshrut['en']['start-date'], '%Y-%m-%d').date()}' AND
+                            "endDate" = '{datetime.strptime(newMarshrut['en']['end-date'], '%Y-%m-%d').date()}' AND
+                            "price" = '{newMarshrut['en']['price']}'""")
+    answer_en = cursor.fetchall()
 
-    keys = int(list(marshrut_bus.keys())[-1]) + 1
+    cursor.execute(f"""SELECT id
+                      FROM ticketbusru
+                      WHERE start = '{newMarshrut['ru']['start']}' AND 
+                            "end" = '{newMarshrut['ru']['end']}' AND
+                            "startDate" = '{datetime.strptime(newMarshrut['ru']['start-date'], '%Y-%m-%d').date()}' AND
+                            "endDate" = '{datetime.strptime(newMarshrut['ru']['end-date'], '%Y-%m-%d').date()}' AND
+                            "price" = '{newMarshrut['ru']['price']}'""")
+    answer_ru = cursor.fetchall()
 
-    marshrut_bus[keys] = {
-            "en": newMarshrut['en'],
-            "ru": newMarshrut['ru']
-        }
-    return marshrut_bus[keys], 201
+    if len(answer_en) > 0 and len(answer_ru) > 0:
+        abort(400, "Маршрут уже создан")
+
+    if len(answer_ru) == 0 and len(answer_en) > 0:
+        try:
+            cursor.execute("SELECT MAX(id) FROM ticketbusru")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+
+            cursor.execute(f"""INSERT INTO ticketbusru(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['ru']['start']}', '{newMarshrut['ru']['end']}',
+                                '{datetime.strptime(newMarshrut['ru']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['ru']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['ru']['start-time']}', '{newMarshrut['ru']['end-time']}',
+                                {newMarshrut['ru']['price'].split(",")[0]})""")
+
+            cn.commit()
+            return newMarshrut['ru'], 201
+        except Exception as e:
+            abort(400, f"Ошибка добавления данных! Ошибка: {e}")
+    
+    if len(answer_ru) > 0 and len(answer_en) == 0:
+        try:
+            cursor.execute("SELECT MAX(id) FROM ticketbusen")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+
+            cursor.execute(f"""INSERT INTO ticketbusru(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['en']['start']}', '{newMarshrut['en']['end']}',
+                                '{datetime.strptime(newMarshrut['en']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['en']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['en']['start-time']}', '{newMarshrut['en']['end-time']}',
+                                {newMarshrut['en']['price'].split(",")[0]})""")
+            cn.commit()
+            return newMarshrut['en'], 201
+        except Exception as e:
+            abort(400, f"Ошибка добавления данных! Ошибка: {e}")
+    else:
+        try:
+            cursor.execute("SELECT MAX(id) FROM ticketbusru")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+            cursor.execute(f"""INSERT INTO ticketbusru(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['ru']['start']}', '{newMarshrut['ru']['end']}',
+                                '{datetime.strptime(newMarshrut['ru']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['ru']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['ru']['start-time']}', '{newMarshrut['ru']['end-time']}',
+                                {newMarshrut['ru']['price'].split(",")[0]})""")
+
+            cursor.execute("SELECT MAX(id) FROM ticketbusen")
+            id = cursor.fetchone()
+            id = 0 if id[0] == None else id[0] + 1
+            cursor.execute(f"""INSERT INTO ticketbusen(id, start, "end", "startDate", "endDate", "startTime", "endTime", price)
+                                VALUES({id}, '{newMarshrut['en']['start']}', '{newMarshrut['en']['end']}',
+                                '{datetime.strptime(newMarshrut['en']['start-date'], '%Y-%m-%d').date()}', '{datetime.strptime(newMarshrut['en']['end-date'], '%Y-%m-%d').date()}',
+                                '{newMarshrut['en']['start-time']}', '{newMarshrut['en']['end-time']}',
+                                {newMarshrut['en']['price'].split(",")[0]})""")
+            cn.commit()
+            return newMarshrut, 201
+        except Exception as e:
+            abort(400, f"Ошибка добавления данных! Ошибка: {e}")
 
 
 def filterTicket_avia(ticket):
     """Функция на тикеты по критерию"""
 
-    language = ["ru", "en"]
-
-    if ticket['language'] not in language:
+    if ticket['language'] not in ['ru', 'en']:
         abort(
             404, f"Язык {ticket['language']} не найден"
         )
 
-    tickets = []
-    for key, value in marshrut_air.items():
-        if ticket['start'] == value[ticket['language']]['start'] and ticket['end'] == value[ticket['language']]['end'] and ticket['date'] == value[ticket['language']]['start-date']:
-            tickets.append(value[ticket['language']])  
+    tickets = {}
+    answer = []
+    
+    try:
+        cursor.execute(f"""SELECT * FROM ticketair{ticket['language']} WHERE 'startDate' = '{ticket['date']}' AND 'end' = '{ticket['end']}' AND 'start' = '{ticket['start']}'""") 
+        
+        answer = cursor.fetchall()
 
-    if len(tickets) > 0:
-        return tickets
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+
+        answer = tickets.values()
+
+    except Exception as e:
+        abort(404, f"Ошибка запроса, повторите запрос позднее. Ошибка: {e}")
+    if len(answer) > 0:
+        return answer
     else:
         abort(404, "Билетов по заданным параметрам не найдено")
 
@@ -253,20 +459,34 @@ def filterTicket_avia(ticket):
 def filterTicket_train(ticket):
     """Функция на тикеты по критерию"""
 
-    language = ["ru", "en"]
-
-    if ticket['language'] not in language:
+    if ticket['language'] not in ['ru', 'en']:
         abort(
             404, f"Язык {ticket['language']} не найден"
         )
 
-    tickets = []
-    for key, value in marshrut_train.items():
-        if ticket['start'] == value[ticket['language']]['start'] and ticket['end'] == value[ticket['language']]['end'] and ticket['date'] == value[ticket['language']]['start-date']:
-            tickets.append(value[ticket['language']])  
+    tickets = {}
+    answer = []
 
-    if len(tickets) > 0:
-        return tickets
+    try:
+        cursor.execute(f"""SELECT * FROM tickettrani{ticket['language']} WHERE 'startDate' = '{ticket['date']}' AND 'end' = '{ticket['end']}' AND 'start' = '{ticket['start']}'""") 
+
+        answer = cursor.fetchall()
+
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+
+        answer = tickets.values()
+
+    except Exception as e:
+        abort(404, f"Ошибка запроса, повторите запрос позднее. Ошибка: {e}")
+    if len(answer) > 0:
+        return answer
     else:
         abort(404, "Билетов по заданным параметрам не найдено")
 
@@ -274,20 +494,34 @@ def filterTicket_train(ticket):
 def filterTicket_bus(ticket):
     """Функция на тикеты по критерию"""
 
-    language = ["ru", "en"]
-
-    if ticket['language'] not in language:
+    if ticket['language'] not in ['ru', 'en']:
         abort(
             404, f"Язык {ticket['language']} не найден"
         )
 
-    tickets = []
-    for key, value in marshrut_bus.items():
-        if ticket['start'] == value[ticket['language']]['start'] and ticket['end'] == value[ticket['language']]['end'] and ticket['date'] == value[ticket['language']]['start-date']:
-            tickets.append(value[ticket['language']])  
+    tickets = {}
+    answer = []
+    
+    try:
+        cursor.execute(f"""SELECT * FROM ticketbus{ticket['language']} WHERE 'startDate' = '{ticket['date']}' AND 'end' = '{ticket['end']}' AND 'start' = '{ticket['start']}'""") 
+        
+        answer = cursor.fetchall()
 
-    if len(tickets) > 0:
-        return tickets
+        for i in answer:
+            tickets[i[0]] = {"Start": i[1],
+                             "End": i[2],
+                             "Start-Date": i[3].isoformat(),
+                             "End-Date": i[4].isoformat(),
+                             "Start-Time": i[5].isoformat(),
+                             "End-Time": i[6].isoformat(),
+                             "Price": i[7]}
+
+        answer = tickets.values()
+
+    except Exception as e:
+        abort(404, f"Ошибка запроса, повторите запрос позднее. Ошибка: {e}")
+    if len(answer) > 0:
+        return answer
     else:
         abort(404, "Билетов по заданным параметрам не найдено")
 
